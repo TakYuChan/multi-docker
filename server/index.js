cbccaba0cc85450e02c8192be4df2833ad5b35c5
TakYuChan <frankychantakyu@yahoo.com.hk>
2020-10-20 23:50:54 +1300

    init

---
+++ server/index.js
@@ -0,0 +1,84 @@
+const keys = require("./keys");
+const { indexAlreadyExists } = require("./utils");
+
+// Express App Setup
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Postgres Client Setup
+const { Pool } = require("pg");
+const pgClient = new Pool({
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort,
+});
+
+pgClient.on("connect", (client) => {
+  client
+    .query("CREATE TABLE IF NOT EXISTS fibdata (fibindices INT PRIMARY KEY)")
+    .catch((err) => console.log(err));
+});
+
+// Redis client setup
+const redis = require("redis");
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
+});
+
+const redisPublisher = redisClient.duplicate();
+
+// Express route handlers
+
+app.get("/", (req, res) => {
+  res.send("Hi");
+});
+
+app.get("/values/all", async (req, res) => {
+  try {
+    const values = await pgClient.query("SELECT * from fibdata");
+    res.send(values.rows);
+  } catch (err) {
+    console.log("##get redis values err", err);
+  }
+});
+
+app.get("/values/current", async (req, res) => {
+  redisClient.hgetall("values", (err, values) => {
+    res.send(values);
+  });
+});
+
+app.post("/values", async (req, res) => {
+  const index = req.body.index;
+
+  if (parseInt(index, 10) > 40) {
+    return res.status(422).send("Index too high");
+  }
+  redisClient.hset("values", index, "Nothing Yet!");
+  // Redis pub/sub not working
+  redisPublisher.publish("insert", index);
+  try {
+    const values = await pgClient.query("SELECT * from fibdata");
+    if (!indexAlreadyExists(values.rows, parseInt(index, 10))) {
+      await pgClient.query("INSERT INTO fibdata(fibindices) VALUES($1)", [
+        parseInt(index, 10),
+      ]);
+    }
+  } catch (err) {
+    console.log("##pg insert err", err);
+  }
+
+  return res.send({ working: true });
+});
+
+app.listen(5000, (err) => {
+  console.log("Listening");
+});
